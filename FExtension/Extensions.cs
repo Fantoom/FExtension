1ccd916185301c64e6fa93c435a71636f8703262
Fantoom <serge.vardanyan00@mail.ru>
2021-03-12 16:20:45 +0400

    Added: Indexed ForEach, For

--- FExtension/Extensions.cs
+++ FExtension/Extensions.cs
@@ -19,6 +19,11 @@ namespace FExtension
         /// <returns>true if the  object returned by selectorA is equal to the object returned by selectorB; otherwise, false</returns>
         public static bool IsEqual<TInA, TInB, ComparableA, ComparableB>(this TInA A, TInB B, Func<TInA, ComparableA> selectorA, Func<TInB, ComparableB> selectorB)
         {
+            if (selectorA is null)
+                throw new ArgumentNullException(nameof(selectorA));
+            if (selectorB is null)
+                throw new ArgumentNullException(nameof(selectorB));
+
             return selectorA(A).Equals(selectorB(B));
         }
         
@@ -34,6 +39,11 @@ namespace FExtension
         /// <returns>Returns a value that indicates whether the object returned by selectorA is equal to object returned by selectorB.</returns>
         public static bool IsEqual<TInA, ComparableA, ComparableB>(this TInA A, Func<TInA, ComparableA> selectorA, Func<ComparableB> selectorB)
         {
+            if (selectorA is null)
+                throw new ArgumentNullException(nameof(selectorA));
+            if (selectorB is null)
+                throw new ArgumentNullException(nameof(selectorB));
+
             return selectorA(A).Equals(selectorB());
         }
        
@@ -51,6 +61,11 @@ namespace FExtension
         /// <returns>A signed integer that indicates the relative position of object returned by selectorA and object returned by selectorB in the sort order</returns>
         public static int CompareSelectedTo<TInA, TInB, ComparableA, ComparableB>(this TInA A, TInB B, Func<TInA, ComparableA> selectorA, Func<TInB, ComparableB> selectorB) where ComparableA : IComparable where ComparableB : IComparable 
         {
+            if (selectorA is null)
+                throw new ArgumentNullException(nameof(selectorA));
+            if (selectorB is null)
+                throw new ArgumentNullException(nameof(selectorB));
+
             return selectorA(A).CompareTo(selectorB(B));
         }
         
@@ -66,6 +81,11 @@ namespace FExtension
         /// <returns>A signed integer that indicates the relative position of object returned by selectorA and object returned by selectorB in the sort order</returns>
         public static int CompareSelectedTo<TInA, ComparableA, ComparableB>(this TInA A,  Func<TInA, ComparableA> selectorA, Func<ComparableB> selectorB) where ComparableA : IComparable where ComparableB : IComparable
         {
+            if (selectorA is null)
+                throw new ArgumentNullException(nameof(selectorA));
+            if (selectorB is null)
+                throw new ArgumentNullException(nameof(selectorB));
+
             return selectorA(A).CompareTo(selectorB());
         }
 
@@ -78,9 +98,77 @@ namespace FExtension
         /// <returns>Source Collection</returns>
         public static IEnumerable<T> ForEach<T>(this IEnumerable<T> source, Action<T> action)
         {
+            if (source is null)
+                throw new ArgumentNullException(nameof(source));
+            if (action is null)
+                throw new ArgumentNullException(nameof(action));
+
             foreach (var item in source)
             {
                 action(item);
+            }
+            return source;
+        }
+        /// <summary>
+        /// Calls action for items in collection 
+        /// </summary>
+        /// <typeparam name="T">Type of item</typeparam>
+        /// <param name="source">Source Collection</param>
+        /// <param name="action">Action that will be called for each item</param>
+        /// <param name="startIndex">Start value of index</param>
+        /// <param name="indexIncrement">The Func to set the way of index incrementation. Default is index++</param>
+        /// <returns>Source Collection</returns>
+        public static IEnumerable<T> ForEach<T>(this IEnumerable<T> source, Action<T,int> action, int startIndex = 0, Func<int,int> indexIncrement = null)
+        {
+            if (source is null)
+                throw new ArgumentNullException(nameof(source));
+            if (action is null)
+                throw new ArgumentNullException(nameof(action));
+
+            int index = startIndex;
+            if (indexIncrement is null)
+            {
+                indexIncrement = x => x++;
+            }
+            foreach (var item in source)
+            {
+                action(item, index);
+                index = indexIncrement(index); 
+            }
+            return source;
+        }
+        /// <summary>
+        /// Calls action for items in collection based on index 
+        /// </summary>
+        /// <typeparam name="T">Type of items</typeparam>
+        /// <param name="source">Source collection</param>
+        /// <param name="action">Action that will be called for each item</param>
+        /// <param name="startIndex">Start value of index</param>
+        /// <param name="indexIncrement">The Func to set the way of index incrementation. Default is index++</param>
+        /// <returns>Source Collection</returns>
+        public static IEnumerable<T> For<T>(this IEnumerable<T> source, Action<T, int> action, int startIndex = 0, Func<int, int> indexIncrement = null)
+        {
+            if (source is null)
+                throw new ArgumentNullException(nameof(source));
+            if (action is null)
+                throw new ArgumentNullException(nameof(action));
+
+            int index = startIndex;
+            int lastIndex = 0;
+            if (indexIncrement is null)
+            {
+                indexIncrement = x => x++;
+            }
+            foreach (var item in source)
+            {
+                if (lastIndex < index - 1)
+                {
+                    lastIndex++;
+                    continue;
+                }
+                action(item, index);
+                lastIndex = index;
+                index = indexIncrement(index);
             }
             return source;
         }
